8d1b37c100b90d3337c37991e0614687279ed7a5
dking3876 <deryk.king@brafton.com>
2016-01-28 18:10:49 -0500

    comment out examples

--- data/controller/DefaultController.php
+++ data/controller/DefaultController.php
@@ -17,16 +17,20 @@ class DefaultController extends OnyxController{
         //echo $this->setting("first_setting");
         $this->model->headerScripts('jquery', 'onyx');
         $this->model->headerScripts('bootstrap', 'onyx');
-        //$this->model->headerScripts(array(
+        /*
+        $this->model->headerScripts(array(
             'name'  => 'testingscript',
             'file'  => 'test.js',
             'type'  => 'external'
             ));
-        //$this->model->styles(array(
+            */
+        /*
+        $this->model->styles(array(
             'name'  => 'welcome',
             'file'  => 'welcome.css',
             'type'  => 'external'
             ));
+            */
         $this->model->styles(array(
             'name'  => 'reset',
             'file'  => 'html5reset.css',
